e185bfc9436e07cb85082ef273e4b6fece8449f5
Harman Ark <ark.harman@gmail.com>
2021-01-26 19:52:17 -0500

    Making progress

--- select.js
+++ select.js
@@ -6,20 +6,43 @@ export default class Select {
         this.labelElement = document.createElement("span")
         this.optionsCustomElement = document.createElement("ul")
         setupCustomElement(this)
+        element.style.display = "none"
         element.after(this.customElement)
+    }
+
+    get selectedOption() {
+        return this.options.find(option => option.selected)
     }
 }
 
 function setupCustomElement(select) {
     select.customElement.classList.add("custom-select-container")
+    select.customElement.tabIndex = 0
 
     select.labelElement.classList.add("custom-select-value")
+    select.labelElement.innerText = select.selectedOption.label
+
     select.customElement.append(select.labelElement)
 
     select.optionsCustomElement.classList.add("custom-select-options")
+    select.options.forEach(option => {
+        const optionElement = document.createElement("li")
+        optionElement.classList.add("custom-select-option")
+        optionElement.classList.toggle("selected", option.selected)
+        optionElement.innerText = option.label
+        optionElement.dataset.value = option.value
+        select.optionsCustomElement.append(optionElement)
+    })
     select.customElement.append(select.optionsCustomElement)
 }
 
 function getFormattedOptions(optionElements) {
-
+    return [...optionElements].map(optionElement => {
+        return {
+            value: optionElement.value,
+            label: optionElement.label,
+            selected: optionElement.selected,
+            element: optionElement
+        }
+    })
 }
